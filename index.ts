55d0ddb3545b15fe4effbe56b93ce3e1af38db0a
antoniosk10 <antoniosk10@gmail.com>
2022-06-17 13:33:20 +0500

    fix: add script multi

--- index.ts
+++ index.ts
@@ -3,23 +3,27 @@ import os from "os";
 import { newServer } from "./server";
 
 if (cluster.isPrimary) {
-  os.cpus().forEach(() => cluster.fork());
-  cluster.on("online", function (worker) {
-    console.log("Worker " + worker.process.pid + " is online");
-  });
+  if (process.argv[2] === "-multi") {
+    os.cpus().forEach(() => cluster.fork());
+    cluster.on("online", function (worker) {
+      console.log("Worker " + worker.process.pid + " is online");
+    });
 
-  cluster.on("exit", function (worker, code, signal) {
-    console.log(
-      "Worker " +
-        worker.process.pid +
-        " died with code: " +
-        code +
-        ", and signal: " +
-        signal
-    );
-    console.log("Starting a new worker");
-    cluster.fork();
-  });
+    cluster.on("exit", function (worker, code, signal) {
+      console.log(
+        "Worker " +
+          worker.process.pid +
+          " died with code: " +
+          code +
+          ", and signal: " +
+          signal
+      );
+      console.log("Starting a new worker");
+      cluster.fork();
+    });
+  } else {
+    newServer();
+  }
 } else {
   newServer();
 }
